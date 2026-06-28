b9d0b6048429ffb4b4ec27a36ad2bec41628fc57
ESKYNA <eskyna.style@gmail.com>
2026-06-28 03:41:26 +0200

    treat secrets secret

--- static/estyleapp/config.example.js
+++ static/estyleapp/config.example.js
@@ -24,8 +24,7 @@
       provider: "firebase-cloud-messaging",
       // Oeffentlicher Web-Push-Zertifikatsschluessel aus Firebase Console > Project settings > Cloud Messaging.
       // Er ist kein Secret und darf im Frontend stehen. Ohne diesen Key kann die App kein FCM Token erstellen.
-      fcmVapidKey:
-        "BHGkQCNERhQvQrVdDR7Vx2zJeolxpnGePy3vEj3j5FmTY0_GjHLZ8ibaupvVShX3OIA95yKDg6BNbWDnmUOJrH0",
+      fcmVapidKey: "",
       // GitHub Pages kann keine /api/fcm/register POST-Endpunkte ausfuehren.
       // Deshalb speichert die App FCM Tokens direkt in Cloud Firestore.
       tokenStorage: "firestore",
@@ -53,13 +52,13 @@
       // Firebase Web App Konfiguration.
       // In Firebase Authentication vorerst nur Google aktivieren; Email/Password deaktiviert lassen.
       firebaseConfig: {
-        apiKey: "AIzaSyBLnOeqtIgBUObt5S4G9vImavaeS0lua1E",
-        authDomain: "eskyna-style.firebaseapp.com",
-        projectId: "eskyna-style",
-        storageBucket: "eskyna-style.firebasestorage.app",
-        messagingSenderId: "349179931593",
-        appId: "1:349179931593:web:332b9c02eaee3e8e525618",
-        measurementId: "G-ERP45XHEG9",
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: "",
       },
     },
   };
